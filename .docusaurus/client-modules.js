97197dda017c5d8678c4a82206e144f289dc9e8a
ConJayTech <conor.teich@gmail.com>
2024-11-12 10:46:43 -0800

    .gitignore works now

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\IQM\\docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\IQM\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\IQM\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\IQM\\docs\\src\\css\\custom.css"),
-];
