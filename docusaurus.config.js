aec3bd785e6840525e9cdba5879c5dc1613eb320
ConJayTech <conor.teich@gmail.com>
2025-01-30 10:51:55 -0800

    Update docusaurus.config.js
    Change autocollapse behavior of sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ const config = {
       docs: {
         sidebar: {
           hideable: false,
-          autoCollapseCategories: true,
+          autoCollapseCategories: false,
         },
       },
       algolia: {
