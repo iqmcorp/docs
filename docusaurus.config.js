b2235073b99ffddfef83b1b6e58955ead455cd71
Conor Teichroeb <103473932+ConJayTech@users.noreply.github.com>
2025-06-06 09:35:14 -0700

    Update docusaurus.config.js
    New Algolia index

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,13 +98,9 @@ const config = {
       },
       algolia: {
         appId: "09FZUVDE53",
-        apiKey: "0a3dbfa566444b62a52591ad5db9b3c8",
-        indexName: "iqmcorpio",
+        apiKey: "***REMOVED***",
+        indexName: "IQM API Docs",
         contextualSearch: true,
-        replaceSearchResultPathname: {
-          from: `/docs/`,
-          to: `/`
-        }
       },
       image: "img/IQM-logo-dark.jpg",
       navbar: {
