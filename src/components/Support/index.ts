dcc97c43234757e60b759e626b21fd45e50c78dc
Conor Teichroeb <conor@iqm.com>
2026-01-23 10:34:57 -0800

    feat: Add community support features and feedback integration
    - Add SupportPanel component (Ask Community + Was this helpful)
    - Add CommunitySection for homepage with 4 community cards
    - Create custom 404 page with community help link
    - Add 'Need help? Ask the community' link to sidebar
    - Integrate FeedbackWidget with GitHub Discussions
    - Add Support section to footer
    - Inject SupportPanel on all doc pages (via DocItem/Footer swizzle)

---
+++ src/components/Support/index.ts
@@ -0,0 +1,4 @@
+export { default as AskQuestion } from './AskQuestion';
+export { default as FeedbackWidget } from './FeedbackWidget';
+export { default as SupportPanel } from './SupportPanel';
+export { default as CommunitySection } from './CommunitySection';
